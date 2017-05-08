ef6209bcac29c312af086538a9bfdd5c2fd1c5e3
NextFuture <546714086@qq,com>
2017-05-08 21:08:17 +0800

    first

--- Module/src/main/java/com/example/module/ModuleMainactivity.java
+++ Module/src/main/java/com/example/module/ModuleMainactivity.java
@@ -17,5 +17,6 @@ public class ModuleMainactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_main);
+
     }
 }
